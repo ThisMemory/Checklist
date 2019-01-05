8e3c50a3a74a2fdb780ed9a08acde467b9b1e469
David Azito <thismemory@icloud.com>
2019-01-05 14:47:56 -0800

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by David Azito on 1/5/19.
+//  Copyright © 2019 David Azito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
